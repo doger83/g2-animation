feb8fdb88b5d4fd1ae0164310930ae463c7d57b7
doger83 <dominic@gerweck2.de>
2023-01-29 02:38:31 +0100

    Add NULL checks

--- g2.Quadtree/PointRegionQuadtree.cs
+++ g2.Quadtree/PointRegionQuadtree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -20,8 +21,8 @@ public class PointRegionQuadtree //: IQuadtree
 
     public Quadrant Boundary { get; }
     public int Capacaty { get; }
-    public List<Point> Points { get; private set; }
     public bool Divided { get; private set; } 
+    public List<Point> Points { get; private set; }
     public PointRegionQuadtree? NorthWest { get; private set; }
     public PointRegionQuadtree? NorthEast { get; private set; }
     public PointRegionQuadtree? SouthEast { get; private set; }
@@ -37,6 +38,7 @@ public class PointRegionQuadtree //: IQuadtree
 
         if (!Divided)
         {
+            // Todo: Points may be null here! 
             if (Points.Count < Capacaty)
             {
                 Points.Add(point);
@@ -45,15 +47,22 @@ public class PointRegionQuadtree //: IQuadtree
             Subdivide();
         }
 
-        return (
-            NorthEast!.Insert(point) ||
-            NorthWest!.Insert(point) ||
-            SouthEast!.Insert(point) ||
-            SouthWest!.Insert(point)
-            );
+        return 
+            (NorthEast?.Insert(point) ?? throw new NullReferenceException($"{nameof(NorthEast)} cannot be null")) ||
+            (NorthWest?.Insert(point) ?? throw new NullReferenceException($"{nameof(NorthWest)} cannot be null")) ||
+            (SouthEast?.Insert(point) ?? throw new NullReferenceException($"{nameof(SouthEast)} cannot be null")) ||
+            (SouthWest?.Insert(point) ?? throw new NullReferenceException($"{nameof(SouthWest)} cannot be null"))
+            ;
     }
 
     private void Subdivide()
+    {
+        InitializeSubQuadrants();
+        MovePointsToChildren();              
+        Divided = true;
+    }
+
+    private void InitializeSubQuadrants()
     {
         var x = Boundary.X;
         var y = Boundary.Y;
@@ -71,13 +80,18 @@ public class PointRegionQuadtree //: IQuadtree
 
         var sw = new Quadrant(x - w / 2, y + h / 2, w / 2, h / 2);
         SouthWest = new(sw, Capacaty);
+    }
 
-        Divided = true;
-
-        // Move points to children.
-        // This improves performance by placing points
-        // in the smallest available rectangle.
-        foreach (var p in Points) {
+    private void MovePointsToChildren()
+    {
+        if (NorthWest is null || NorthEast is null || SouthEast is null || SouthWest is null)
+        {
+            throw new NullReferenceException("Children must be initialized before you can move points into them! Hint: maybe you changed order in methodcalls?");
+        }
+        // This improves performance by placing points in the smallest available rectangle.
+        for (int i = 0; i < Points.Count; i++)
+        {
+            Point? p = Points[i];
             var inserted =
                 NorthWest.Insert(p) ||
                 NorthEast.Insert(p) ||
