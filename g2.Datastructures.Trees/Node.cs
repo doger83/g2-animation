83147160461bda3678f6857d6623c6f5881b59bf
doger83 <dominic@gerweck2.de>
2023-01-31 15:41:38 +0100

    renaming

---
+++ g2.Datastructures.Trees/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace g2.Quadtree;
+
+//public struct Node
+//{
+//    private Point _point;
+//    private int _value;
+
+//    public Node()
+//    {
+//        _point = new Point();
+//        _value = 0;
+//    }
+
+//    public Node(Point point, int value)
+//    {
+//        _point = point;
+//        _value = value;
+//    }
+//}
