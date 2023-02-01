d9aa30e5c727f81cde58bddef93f8614fa2df482
doger83 <dominic@gerweck2.de>
2023-02-01 01:23:52 +0100

    Add Node class

--- g2.Quadtree/Node.cs
+++ g2.Quadtree/Node.cs
@@ -4,22 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace g2.Quadtree;
+namespace g2.Quadtree;s
 
-//public struct Node
-//{
-//    private Point _point;
-//    private int _value;
+public struct Node
+{
+    private Point _point;
+    private int _value;
 
-//    public Node()
-//    {
-//        _point = new Point();
-//        _value = 0;
-//    }
+    public Node()
+    {
+        _point = new Point();
+        _value = 0;
+    }
 
-//    public Node(Point point, int value)
-//    {
-//        _point = point;
-//        _value = value;
-//    }
-//}
+    public Node(Point point, int value)
+    {
+        _point = point;
+        _value = value;
+    }
+}
