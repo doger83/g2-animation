d127a41fc093a9bb405af86e3b128da13f6ed8ee
doger83 <dominic@gerweck2.de>
2023-02-01 01:22:37 +0100

    Add Time class with deltatime

---
+++ g2.QuadTree.DesktopWPFUI/Time.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace g2.Datastructures.DesktopWPFUI;
+
+public static class Time
+{
+    private static readonly Stopwatch stopwatch = new Stopwatch();
+    public static double DeltaTime()
+    {
+        double deltaTime = stopwatch.Elapsed.TotalSeconds;
+        stopwatch.Restart();
+        return deltaTime;
+    }
+}
+
