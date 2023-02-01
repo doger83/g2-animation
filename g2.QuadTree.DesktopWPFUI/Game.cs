74cbc7882c9010053ecb5da244a3f62da2d09c62
doger83 <dominic@gerweck2.de>
2023-02-01 01:22:55 +0100

    Add Game class with loop

---
+++ g2.QuadTree.DesktopWPFUI/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace g2.Datastructures.DesktopWPFUI;
+
+public class Game
+{
+    private readonly FPSCounterViewModel fpsCounter;
+    private Task? loop;
+
+    public Game(FPSCounterViewModel fpsCounter)
+    {
+        this.fpsCounter = fpsCounter;
+    } 
+
+    public void Update()
+    {
+        loop = Task.Run( () =>
+        {
+            while (true)
+            {
+                fpsCounter.Draw();
+
+                // Perform game logic here
+              
+                
+                StopGameLoop(100);
+            }
+        });
+    }  
+
+    private void StopGameLoop()
+    {     
+        loop?.Wait();
+    }
+
+    private void StopGameLoop(int milliseconds)
+    {
+        loop?.Wait(milliseconds);
+    }
+}
+
