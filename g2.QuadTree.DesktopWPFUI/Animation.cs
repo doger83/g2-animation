112b2080775d56ecb1f7ff54af99c07072fc4f8e
doger83 <dominic@gerweck2.de>
2023-02-06 06:10:31 +0100

    Refactoring DeltaTime

--- g2.QuadTree.DesktopWPFUI/Animation.cs
+++ g2.QuadTree.DesktopWPFUI/Animation.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace g2.Datastructures.DesktopWPFUI;
 
@@ -20,29 +19,16 @@ public class Animation
 
     public void Update()
     {
-
-        //Time.PrevousTime = Time.stopwatch2.Elapsed;
         while (true)
         {
-            Time.DT = (Time.stopwatch2.Elapsed - Time.PrevousTime).TotalSeconds;
-            Time.PrevousTime = Time.stopwatch2.Elapsed;
-            //Time.Restart();
+            Time.Delta();
 
             fpsCounter.Draw();
             particle.Move();
             particle.Boundary();
-            // ...
-            //Thread.Sleep(10);
-            //Debug.WriteLine($"old {Time.Duration} | {Time.DeltaTime}");
-            //Debug.WriteLine($"new {Time.PrevousTime} | {Time.DT:G35}");
-            //Time.Stop();
-            //Time.Duration = TicksInMilliseconds();
+
 
         }
     }
-
-    private static double TicksInMilliseconds() =>
-        // ToDo: What happens if timer still runs? 
-        (double)Time.stopwatch.ElapsedTicks / (double)Stopwatch.Frequency * 1000.0;
 }
 
--- g2.QuadTree.DesktopWPFUI/Time.cs
+++ g2.QuadTree.DesktopWPFUI/Time.cs
@@ -5,39 +5,18 @@ namespace g2.Datastructures.DesktopWPFUI;
 
 public static class Time
 {
-    public static readonly Stopwatch stopwatch = new();
-    public static readonly Stopwatch stopwatch2 = Stopwatch.StartNew();
+    private static TimeSpan prevousTime;
+    private static readonly Stopwatch stopwatch = Stopwatch.StartNew();
+    public static double DeltaTime { get; private set; }
 
-
-    public static double Duration { get; set; }
-
-    public static double DeltaTime
+    public static void Delta()
     {
-        get
-        {
-            if (Duration == 0) // should not happen. but I want to be save!
-            {
-                return 0;
-            }
-            // The expression 1 / (1000 / x) can be simplified to x / 1000.
-            // double fps = 1000.0 / Duration;        
-            // double deltaTime = 1.0 / fps;          
-            //         1000 
-            return Duration / 1000;
-        }
+        DeltaTime = (stopwatch.Elapsed - prevousTime).TotalSeconds;
+        prevousTime = stopwatch.Elapsed;
     }
 
-    public static TimeSpan PrevousTime { get; internal set; }
-    public static double DT { get; internal set; }
+    private static double TotalTicksInMilliseconds() =>
 
-    public static void Start() => stopwatch.Start();
-
-    public static void Reset() => stopwatch.Reset();
-
-    public static void Restart() => stopwatch.Restart();
-
-    public static void Stop() => stopwatch.Stop();
-
-
+        (double)stopwatch.ElapsedTicks / (double)Stopwatch.Frequency * 1000.0;
 }
 
