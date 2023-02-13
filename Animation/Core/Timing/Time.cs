9a98d39055c92f602001069e4674557373d257d1
doger83 <dominic@gerweck2.de>
2023-02-13 22:50:12 +0100

    Add Timer for fixedUpdate callss

--- Animation/Core/Timing/Time.cs
+++ Animation/Core/Timing/Time.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace g2.Animation.Core.Timing;
 
@@ -7,6 +9,7 @@ public static class Time
     private static Stopwatch? watch;
     private static double deltaTime;
     private static long previousTicks;
+    private static Timer? timer;
 
     public static void Delta()
     {
@@ -25,18 +28,44 @@ public static class Time
         private set => deltaTime = value;
     }
 
-    public static void Reset()
+    public static void ResetWatch()
     {
         watch = null;
     }
 
-    public static void Start()
+    public static void StartWatch()
     {
         if (watch == null)
         {
             watch = new();
             watch.Start();
             previousTicks = watch.ElapsedTicks;
+        }
+    }
+
+
+    public static event EventHandler<EventArgs>? TimerTick;
+
+    private static void OnTimerElapsed(object sender, ElapsedEventArgs e)
+    {
+        TimerTick?.Invoke(sender, e);
+    }
+
+    public static void StartTimer(double interval)
+    {
+        timer = new Timer(interval);
+        timer.Elapsed += OnTimerElapsed!;
+        timer.AutoReset = true;
+        timer.Enabled = true;
+    }
+
+
+    public static void StopTimer()
+    {
+        if (timer != null)
+        {
+            timer.Enabled = false;
+            timer = null;
         }
     }
 }
