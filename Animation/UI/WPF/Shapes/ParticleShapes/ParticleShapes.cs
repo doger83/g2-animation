375049f6d258a38d331af94509ae81a503a80d38
doger83 <dominic@gerweck2.de>
2023-02-14 02:28:45 +0100

    Brush for Stroke frozen

--- Animation/UI/WPF/Shapes/ParticleShapes/ParticleShapes.cs
+++ Animation/UI/WPF/Shapes/ParticleShapes/ParticleShapes.cs
@@ -5,23 +5,27 @@ namespace g2.Animation.UI.WPF.Shapes.Library.ParticleShapes;
 
 public static class ParticleShapes
 {
+
     private static readonly RadialGradientBrush gradientBrush = new()
     {
         GradientStops = { new GradientStop(Colors.LightGray, 0), new GradientStop(Colors.Black, 1) },
-
-
     };
+
+    private static readonly SolidColorBrush strokeBrush = new(Color.FromArgb(255, 100, 100, 205));
+
+    static ParticleShapes()
+    {
+        gradientBrush.Freeze();
+        strokeBrush.Freeze();
+    }
+
     public static Ellipse CircleBasis(double radius)
     {
-
-        gradientBrush.Freeze();
-
-
         return new()
         {
             Width = radius * 2,
             Height = radius * 2,
-            Stroke = new SolidColorBrush(Color.FromArgb(255, 100, 100, 205)),
+            Stroke = strokeBrush,
             StrokeThickness = 1,
             Fill = gradientBrush,
         };
