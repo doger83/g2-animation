d93a8d4fa62918ee7ee9b03c3b99e562f48c7692
doger83 <dominic@gerweck2.de>
2023-02-19 00:10:58 +0100

    particles stored in array

--- Animation/Core/AnimationSystems/AnimationBase.cs
+++ Animation/Core/AnimationSystems/AnimationBase.cs
@@ -1,16 +1,15 @@
 ﻿using g2.Animation.Core.ParticleSystems;
 using g2.Animation.Core.Timing;
 using g2.Datastructures.Geometry;
-using NetTopologySuite.Mathematics;
 
 namespace g2.Animation.Core.AnimationSystems;
 
 public class AnimationBase
 {
     private readonly FPSCounter fpsCounter;
-
     private readonly Quadrant quadrant;
-    private readonly List<Particle> particles;
+    private readonly Particle[] particles;
+    private int particlesCount = 1000;
 
     private bool stopThread;
 
@@ -22,31 +21,29 @@ public class AnimationBase
         // ToDo: Add Boundary for canvas maybe move chicking for boundaries in box like quadtree?  or BoundaryCheckc class?  efficiant boundary checks (k d tree?)
 
 
-        particles = new List<Particle>();
+        particles = new Particle[particlesCount];
 
         Random random = new();
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < particlesCount; i++)
         {
             double x = random.NextDouble() * width;
             double y = random.NextDouble() * height;
-            Particle particle = new(x, y, 25, quadrant)
+            Particle particle = new(x, y, 2, quadrant)
             {
-                Speed = new Vector2D((random.NextDouble() * 150) - 75, (random.NextDouble() * 100) - 75)
+                Speed = new Vector2D((random.NextDouble() * 150) - 75, (random.NextDouble() * 150) - 75)
 
-                //Speed = new Vector2(50, 0)
-
+                //Speed = new Vector2D(200, 0)
             };
 
-            particles.Add(particle);
+            particles[i] = particle;
         }
     }
 
-    public List<Particle> Particles => particles;
+    public Particle[] Particles => particles;
 
     public Task Update()
     {
-
         // ToDo: Make tis a thread? return?
         return Task.Run(() =>
         {
@@ -60,7 +57,7 @@ public class AnimationBase
 
                 fpsCounter.UpdateContent();
 
-                for (int i = 0; i < particles.Count; i++)
+                for (int i = 0; i < particles.Length; i++)
                 {
                     particles[i].Move();
                     particles[i].Boundary();
@@ -78,7 +75,6 @@ public class AnimationBase
 
             Time.ResetWatch();
         });
-
     }
 
     public void StopThread()
