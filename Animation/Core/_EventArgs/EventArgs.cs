c9bcf434446e2f85bf39e1d120b383add26236f0
doger83 <dominic@gerweck2.de>
2023-02-22 02:51:04 +0100

    Add HiReSTimer

---
+++ Animation/Core/_EventArgs/EventArgs.cs
@@ -0,0 +1,51 @@
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: EventArgs.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2021 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution.
+//
+//  Please refer to the LICENSE file if you want to use this source code.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+#region Usings
+
+using System;
+
+#endregion
+
+namespace g2.Animation.Core._EventArgs;
+
+/// <summary>
+/// Represents a simple event argument of type <typeparamref name="T"/>.
+/// </summary>
+/// <typeparam name="T">The type of the event argument.</typeparam>
+public class EventArgs<T> : EventArgs
+{
+    #region Properties
+
+    /// <summary>
+    /// Gets the event data.
+    /// </summary>
+    public T EventData { get; }
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EventArgs{T}"/> class.
+    /// </summary>
+    /// <param name="arg">The argument.</param>
+    public EventArgs(T arg)
+    {
+        EventData = arg;
+    }
+
+    #endregion
+}
---
+++ Animation/Core/_EventArgs/HiResTimerElapsedEventArgs.cs
@@ -0,0 +1,55 @@
+﻿#region Copyright
+
+///////////////////////////////////////////////////////////////////////////////
+//  File: HiResTimerElapsedEventArgs.cs
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) KGy SOFT, 2005-2021 - All Rights Reserved
+//
+//  You should have received a copy of the LICENSE file at the top-level
+//  directory of this distribution.
+//
+//  Please refer to the LICENSE file if you want to use this source code.
+///////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+#region Usings
+
+using System;
+
+#endregion
+
+namespace g2.Animation.Core._EventArgs;
+
+/// <summary>
+/// Provides data for the <see cref="HiResTimer.Elapsed">HiResTimer.Elapsed</see> event.
+/// </summary>
+public class HiResTimerElapsedEventArgs : EventArgs
+{
+    #region Properties
+
+    /// <summary>
+    /// Gets the delay, in milliseconds, of the triggering of the <see cref="HiResTimer.Elapsed">HiResTimer.Elapsed</see> event
+    /// compared to when it should have been called.
+    /// </summary>
+    public float Delay { get; }
+
+    /// <summary>
+    /// Gets the number of the fallen out <see cref="HiResTimer.Enabled">HiResTimer.Enabled</see> events since the last invoke.
+    /// The value is nonzero if a larger delay occurred than the value of the <see cref="HiResTimer.IgnoreElapsedThreshold">HiResTimer.IgnoreElapsedThreshold</see> property
+    /// and thus one or more <see cref="HiResTimer.Elapsed">HiResTimer.Elapsed</see> events were skipped to catch up the timer.
+    /// </summary>
+    public int Fallouts { get; }
+
+    #endregion
+
+    #region Constructors
+
+    internal HiResTimerElapsedEventArgs(float delay, int fallouts)
+    {
+        Delay = delay;
+        Fallouts = fallouts;
+    }
+
+    #endregion
+}
