a511d34d08f1419969ae6305c8606b8309583b91
doger83 <dominic@gerweck2.de>
2023-03-02 01:20:00 +0100

    gedoens

--- Animation/TestWPFDesktopApp/MainWindow.xaml.cs
+++ Animation/TestWPFDesktopApp/MainWindow.xaml.cs
@@ -65,6 +65,8 @@ public partial class MainWindow : Window
     private void UpdateFPS(object? sender, EventArgs e)
     {
         viewModel.Update();
+
+        mainCanvas.InvalidateVisual();
     }
 
     private bool started;
@@ -89,18 +91,14 @@ public partial class MainWindow : Window
         for (int i = 0; i < animation!.Particles.Length; i++)
         {
 
-            //canvasParticles![i].Shape.Arrange(new Rect(
-            //    animation.Particles[i].X - animation.Particles[i].Width, 
-            //    animation.Particles[i].Y - animation.Particles[i].Height, 
-            //    canvasParticles![i].Shape.ActualWidth, 
-            //    canvasParticles![i].Shape.ActualHeight));
+            //Canvas.SetLeft(canvasParticles?[i].Shape, animation.Particles[i].X - animation.Particles[i].Width);
+            //Canvas.SetTop(canvasParticles?[i].Shape, animation.Particles[i].Y - animation.Particles[i].Height);
 
             canvasParticles![i].Shape.SetValue(Canvas.LeftProperty, animation.Particles[i].X - animation.Particles[i].Width);
             canvasParticles![i].Shape.SetValue(Canvas.TopProperty, animation.Particles[i].Y - animation.Particles[i].Height);
 
             //Debug.WriteLine($"UI X:\t{animation?.Particles[i].Position.X}\tXSpeed:\t{animation?.Particles[i].XSpeed}\tdt:\t{Time.DeltaTime:G65}");
         }
-        mainCanvas.InvalidateVisual();
 
         return Task.CompletedTask;
     }
