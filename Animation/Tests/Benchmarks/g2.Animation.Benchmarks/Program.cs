317abf7f8a21cc5b2661b7a27532c10e9e15b0a1
doger83 <dominic@gerweck2.de>
2023-03-06 01:50:47 +0100

    delete Tests (only in Development branch)

--- Animation/Tests/Benchmarks/g2.Animation.Benchmarks/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Exporters.Csv;
-using BenchmarkDotNet.Running;
-using g2.Animation.Benchmarks.ParticleSystems;
-
-var summary = BenchmarkRunner.Run<ParticleBenchmarks>();
