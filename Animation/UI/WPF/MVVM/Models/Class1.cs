29996fe1129c88e3019cebdd6e7eefd23c1f2c77
doger83 <dominic@gerweck2.de>
2023-03-07 23:52:10 +0100

    restructure projects and solutions

--- Animation/UI/WPF/MVVM/Models/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace g2.Animation.UI.Models.Library;
-public class Class1
-{
-
-}
---
+++ Animation/tests/Benchmarks/g2.Animation.Benchmarks/UI/UIBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace g2.Animation.Core.Benchmarks.UI;
+
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class UIBenchmarks
+{
+    [Benchmark(Baseline = true)]
+    public void UpdateUI_basic()
+    {
+
+    }
+
+
+}
